0debc35848fb2d8d9e764c7474857c586dce95fe
Youngsun <lsun@live.cn>
2018-10-16 14:01:54 +0800

    Add HttpRequest.Get()

---
+++ Wallet/HttpRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Wallet {
+    class HttpRequest {
+        HttpClient httpClient = new HttpClient();
+        public JObject Get(string url) {
+            string json = httpClient.GetStringAsync(url).Result;
+            return JObject.Parse(json);
+        }
+    }
+}
