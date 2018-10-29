d3934d596f9955306aace2258043910f9f8bed58
Youngsun <lsun@live.cn>
2018-10-29 19:23:36 +0800

    Add Post

--- Wallet/HttpRequest.cs
+++ Wallet/HttpRequest.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
 
 namespace Wallet {
@@ -6,6 +8,12 @@ namespace Wallet {
         HttpClient httpClient = new HttpClient();
         public JObject Get(string url) {
             string json = httpClient.GetStringAsync(url).Result;
+            return JObject.Parse(json);
+        }
+        public JObject Post(string boby, string url = "http://127.0.0.1:20337") {
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage httpResponseMessage = httpClient.PostAsync(url, new StringContent(boby)).Result;
+            string json = httpResponseMessage.Content.ReadAsStringAsync().Result;
             return JObject.Parse(json);
         }
     }
