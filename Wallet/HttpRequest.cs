30c99afc24a41f5644fbd948aadc9eaa24d46480
Youngsun <lsun@live.cn>
2018-10-30 17:23:52 +0800

    using HttpClient

--- Wallet/HttpRequest.cs
+++ Wallet/HttpRequest.cs
@@ -3,22 +3,27 @@ using Newtonsoft.Json.Linq;
 
 namespace Wallet {
     class HttpRequest {
-        HttpClient httpClient = new HttpClient();
         public JObject Get(string _method, string _params, int _id = 1, string _node = "http://212.64.42.147:40332") {
-            string url = _node + "/?jsonrpc=2.0&method=" + _method + "&params=[\"" + _params + "\"]&id=" + 1;
-            string json = httpClient.GetStringAsync(url).Result;
-            return JObject.Parse(json);
+            using(HttpClient httpClient = new HttpClient()) {
+                string url = _node + "/?jsonrpc=2.0&method=" + _method + "&params=[\"" + _params + "\"]&id=" + 1;
+                string result = httpClient.GetStringAsync(url).Result;
+                return JObject.Parse(result);
+            }
         }
         public JObject Post(string _method, string _params, int _id = 1, string _node = "http://212.64.42.147:40332") {
-            string body = "{\"jsonrpc\": \"2.0\",\"method\": \"" + _method + "\",\"params\": [\"" + _params + "\"],\"id\": " + _id + "}";
-            HttpResponseMessage httpResponseMessage = httpClient.PostAsync(_node, new StringContent(body)).Result;
-            string json = httpResponseMessage.Content.ReadAsStringAsync().Result;
-            return JObject.Parse(json);
+            using(HttpClient httpClient = new HttpClient()) {
+                string body = "{\"jsonrpc\": \"2.0\",\"method\": \"" + _method + "\",\"params\": [\"" + _params + "\"],\"id\": " + _id + "}";
+                HttpResponseMessage httpResponseMessage = httpClient.PostAsync(_node, new StringContent(body)).Result;
+                string result = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                return JObject.Parse(result);
+            }
         }
-        public JObject GetBlock(long blockNum, string node = "http://212.64.42.147:40332") {
-            string url = node + "/?jsonrpc=2.0&method=getblock&params=[" + blockNum + ",1]&id=" + 1;
-            string json = httpClient.GetStringAsync(url).Result;
-            return JObject.Parse(json);
+        public JObject GetBlock(long _blockNum, string _node = "http://212.64.42.147:40332") {
+            using(HttpClient httpClient = new HttpClient()) {
+                string url = _node + "/?jsonrpc=2.0&method=getblock&params=[" + _blockNum + ",1]&id=" + 1;
+                string result = httpClient.GetStringAsync(url).Result;
+                return JObject.Parse(result);
+            }
         }
     }
 }
